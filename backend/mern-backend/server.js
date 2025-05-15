6163c8a02071ec461f872a854622df37ac1971ba
Kai-Barker <241065@virtualwindow.co.za>
2025-05-15 12:01:09 +0200

    more setup, help

--- backend/mern-backend/server.js
+++ backend/mern-backend/server.js
@@ -2,13 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+//load enviornment variables from .env file
 dotenv.config();
 
+//Initialise express app
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
+//Middleware to parse JSON bodies
 app.use(express.json());
 
+//MONGODB connection 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
@@ -16,3 +21,16 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+//User Routes
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then( () => console.log('MongoDB connected'))
+.catch(err => console.error(err));
+
+
+//Start the server
+const userRoutes = require('./routes/userRoutes');
+app.use('/api/users', userRoutes)
